70feaf34bc675761d1711073f88e84c80ed8cdf8
everduo <316579182@qq.com>
2020-12-01 21:39:05 +0800

    redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
-import {createStore} from 'redux';
-import reducer from './reducer'
-const store = createStore(reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+import { createStore , applyMiddleware ,compose } from 'redux'  //  引入createStore方法
+import reducer from './reducer'    
+import thunk from 'redux-thunk'
+// import createSagaMiddleware from 'redux-saga'
 
-export default store
+// const sagaMiddleware = createSagaMiddleware()
+
+const composeEnhancers =   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}):compose
+
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+
+const store = createStore( reducer, enhancer) // 创建数据存储仓库
+export default store   //暴露出去
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,4 @@
-import {CHANGE_INPUT,ADD_ITEM,DELETE_ITEM} from './constant'
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM, GET_LIST } from './constant'
 const defaultState = {
   inputValue: 'Write',
   list: [
@@ -9,23 +9,29 @@ const defaultState = {
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (state = defaultState,action)=>{
-  if(action.type === CHANGE_INPUT){
-      let newState = JSON.parse(JSON.stringify(state)) //深度拷贝state
-      newState.inputValue = action.value
-      return newState
+export default (state = defaultState, action) => {
+  if (action.type === CHANGE_INPUT) {
+    let newState = JSON.parse(JSON.stringify(state)) //深度拷贝state
+    newState.inputValue = action.value
+    return newState
   }
   //state值只能传递，不能使用
-  if(action.type === ADD_ITEM ){ //根据type值，编写业务逻辑
-      let newState = JSON.parse(JSON.stringify(state)) 
-      newState.list.push(newState.inputValue)  //push新的内容到列表中去
-      newState.inputValue = ''
-      return newState
+  if (action.type === ADD_ITEM) { //根据type值，编写业务逻辑
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)  //push新的内容到列表中去
+    newState.inputValue = ''
+    return newState
   }
-  if(action.type === DELETE_ITEM ){ //根据type值，编写业务逻辑
-      let newState = JSON.parse(JSON.stringify(state)) 
-      newState.list.splice(action.index,1)  //push新的内容到列表中去
-      return newState
+  if (action.type === DELETE_ITEM) { //根据type值，编写业务逻辑
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list.splice(action.index, 1)  //push新的内容到列表中去
+    return newState
+  }
+
+  if (action.type === GET_LIST) { //根据type值，编写业务逻辑
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list = action.data.data.list //复制性的List数组进去
+    return newState
   }
   return state
 }
